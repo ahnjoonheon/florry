8d734f62b5f7e2fb86f0b002bc735a806f8cf6c7
안준헌 <universal304015@alltmro.com>
2022-08-25 14:09:47 +0900

    UserApiApplication.java Path 변경 - jpa bean scan 관련 이슈 해결

--- florry-client-user-api/src/main/java/com/florry/UserApiApplication.java
+++ florry-client-user-api/src/main/java/com/florry/UserApiApplication.java
@@ -1,11 +1,9 @@
-package com.florry.user;
+package com.florry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-//@EntityScan(basePackages = "com.florry.domain")
-//@EnableJpaRepositories(basePackages = "com.florry.domain")
 @EnableJpaAuditing
 @SpringBootApplication
 public class UserApiApplication {
